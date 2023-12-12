9b61f764a099f34cdd3eb2f5ee0ecdee42469524
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-12 10:22:59 -0300

    feat: class

---
+++ src/01-class.ts
@@ -0,0 +1,15 @@
+class MyDate {
+  year:number;
+  month:number;
+  day:number;
+
+  constructor(year:number, month:number, day:number) {
+    this.year = year
+    this.month = month
+    this.day = day
+  }
+}
+
+const myDate = new MyDate(1999,2,7);
+
+console.log(myDate);
