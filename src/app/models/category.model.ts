39dd45bd7f80f2b9253fb70b9ed424469c618b04
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-14 22:05:08 -0300

    feat: decorators

--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,5 +1,12 @@
+export enum AccessType {
+  PRIVATE = 'private',
+  PUBLIC = 'public'
+}
+
+
 export interface Category {
   id:         number | string;
   name:       string;
   image:      string;
+  access?:    AccessType;
 }
