693085ccc6653c4f3c19e2267389b6ab5cb5f169
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-14 21:51:31 -0300

    feat: generics en metodos

--- src/app/services/base-http.service.ts
+++ src/app/services/base-http.service.ts
@@ -1,24 +1,33 @@
 import axios from "axios";
 import { Category } from "../models/category.model"
 import { Product } from "../models/product.model";
+import { UpdateProductDto } from "../dtos/product.dto";
 export class BaseHttpService<T> {
 
   constructor(
-    private url: string
+    protected url: string
   ){}
 
   async getAll(): Promise<T[]> {
     const { data } = await axios.get<T[]>(this.url);
     return data;
   }
+
+  async update<ID,DTO>(id: ID, changes: DTO) {
+    const {data} = await axios.put<Product>(`${this.url}/${id}`, changes);
+    return data;
+  }
 }
 
 (async() => {
   const url1 = 'https://api.escuelajs.co/api/v1/products'
-  const service = new BaseHttpService<Product>(url1);
-  console.log('products',(await service.getAll()).length)
+  const productService = new BaseHttpService<Product>(url1);
+  console.log('products',(await productService.getAll()).length)
+  productService.update<Product['id'], UpdateProductDto>(1,{
+    title:'as',
+  })
 
   const url2 = 'https://api.escuelajs.co/api/v1/categories'
-  const service2 = new BaseHttpService<Product>(url2);
-  console.log('categories',(await service2.getAll()).length)
+  const categoryService = new BaseHttpService<Category>(url2);
+  console.log('categories',(await categoryService.getAll()).length)
 })()
